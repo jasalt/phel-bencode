9e8d0dd614897a64555654a81324772daaa88f0e
Jarkko Saltiola <jarkko@saltiolacode.fi>
2025-03-04 17:54:06 +0200

    upgrade Phel 0.16.1

--- phel-config.php
+++ phel-config.php
@@ -2,15 +2,6 @@
 
 declare(strict_types=1);
 
-return [
-    'src-dirs' => ['src'],
-    'test-dirs' => ['tests'],
-
-    # TODO: Will be made redundant in the next version of Phel. Remove then.
-    'vendor-dir' => 'vendor',
-    'export' => [
-        'directories' => ['src/modules'],
-        'namespace-prefix' => 'PhelGenerated',
-        'target-directory' => 'src/PhelGenerated',
-    ],
-];
+return (new \Phel\Config\PhelConfig())
+    ->setSrcDirs(['src'])
+    ->setTestDirs(['tests']);
